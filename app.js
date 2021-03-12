d88478f73df7550e3fe6c2e0dce67366f99317cc
Ken Robinson <subscriptions@kodehaus.com>
2021-03-11 23:41:53 -0500

    errs

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log('------------------------------------------')
+  console.log(err.status)
+  console.log(err.message)
+  console.log(err.stack)
+  console.log('------------------------------------------')
   // render the error page
   res.status(err.status || 500);
   if(res.statusCode == '404'){
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ const { Book } = db;
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+  res.redirect('/books');
+});
+
+
+router.get('/books', async function(req, res, next) {
   let books = await Book.findAll();
-  res.render('allBooks', books);
+  res.render('allBooks', {books: books});
 });
 
 module.exports = router;
